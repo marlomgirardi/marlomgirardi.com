b708744fdd7d31c3229c36774e04c230f7b37a92
Marlom Girardi <marlom.girardi@gmail.com>
2023-07-19 22:16:00 +0100

    move to next

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div className="w-max[300px]">
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-400 to-zinc-50">
+        Marlom Girardi
+      </h1>
+      <p className="mt-6">
+        <strong>
+          Senior Front-End Engineer at{" "}
+          <a href="https://www.rvvup.com" rel="nofollow noopener">
+            Rvvup
+          </a>
+        </strong>
+        <br />
+        <span className="leading-9">Creating awesome experiences</span>
+      </p>
+    </div>
+  );
+}
--- src/components/WrapPageElement.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Locale } from "./Locale"
-
-const WrapPageElement = ({ element, props }) => (
-  <Locale {...props}>{element}</Locale>
-)
-
-export default WrapPageElement
