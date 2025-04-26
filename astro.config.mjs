b84b1152c422a67b2f4719d0c9f64840f4435012
Marlom Girardi <hi@marlomgirardi.com>
2025-04-26 01:15:48 +0100

    upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
